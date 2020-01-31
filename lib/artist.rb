228648ec7f8fc293d98158affefe86d5cfef80a2
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-01-31 09:54:52 -0800

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,10 @@
+class Artist
+
+
+def name
+    @artist
+end
+def name=(artist)
+    @artist=artist
+end
+end
---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+    def title
+        @title
+    end
+    def title=(title)
+        @title=title
+    end
+    def artist=(author)
+        @author=author
+    end
+
+def artist
+    @author
+end
+
+
+end
